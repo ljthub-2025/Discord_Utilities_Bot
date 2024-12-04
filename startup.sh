3f8e577798987157ff3918ec369af3f399af1bf6
LJT1109 <ljt.tw@outlook.com>
2024-12-04 22:45:24 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# # Create and activate virtual environment if it doesn't exist
-# if [ ! -d "venv" ]; then
-#     python -m venv venv
-# fi
-
-# # Activate virtual environment
-# source venv/bin/activate
+# Initialize conda for bash
+eval "$(conda shell.bash hook)"
 
 #use conda
 conda activate discord_utilities_bot
