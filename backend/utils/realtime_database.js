0d04461a8e24f80d80397db4c890392dc964b14d
LJT1109 <ljt.tw@outlook.com>
2024-12-06 00:57:20 +0800

    Update realtime_database.js

--- backend/utils/realtime_database.js
+++ backend/utils/realtime_database.js
@@ -1,8 +1,9 @@
 const { ref, get, onValue, set } = require("firebase/database");
 const { db } = require('./firebase.js');
+const { db_root } = require('../config.json');
 
 const setGuild = async (guild) => {
-    const guildRef = ref(db, 'guilds/' + guild.id);
+    const guildRef = ref(db,  db_root + '/guilds/' + guild.id);
     set(guildRef, {
         name: guild.name,
     });
